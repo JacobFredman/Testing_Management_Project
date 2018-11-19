69991589140d1e606f764f11d5bca8818cf6dfec
jacob <jacob@app2q.com>
2018-11-19 23:32:47 +0200

    DS project added

---
+++ DAL/AccessTraineesData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using  BE;
+using  DS;
+
+namespace DAL
+{
+
+    public class AccessTraineesData
+    {
+        private Data data = new Data();
+        //    private List <Trainee> _trainees = new List<Trainee>();
+
+        public void Update(Trainee updatedTrainee)
+        {
+            var trainee = da _trainees.Find(t => t.ID == updatedTrainee.ID);
+            _trainees.Remove(trainee);
+            _trainees.Add(updatedTrainee);
+        }
+
+        public void Delete(Trainee traineeToDelete)
+        {
+            _trainees.Remove(traineeToDelete);
+        }
+
+    }
+
+
+
+
+
+
+
+
+}
--- DS/Data.cs
+++ DS/Data.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BE;
 
 namespace DS
 {
-    public class Class1
+    public class Data
     {
+        private List<Trainee> _trainees = new List<Trainee>();
     }
+
 }
