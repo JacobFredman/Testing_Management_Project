8a3be084f9b242a5fcaa9eef9c776694ad3e1dd4
jacob <jacob@app2q.com>
2018-11-21 12:15:14 +0200

    Idal added

---
+++ DAL/Idal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using DS;
+
+namespace DAL
+{
+    interface IDal
+    {
+        void AddTester(Tester t);
+        void RemoveTester(Tester t);
+        void UpdateTester(Tester t);
+
+        void AddTest(Test t);
+        void RemoveTest(Test t);
+        void UpdateTest(Test t);
+
+        void AddTrainee(Trainee t);
+        void RemoveTrainee(Trainee t);
+        void UpdateTrainee(Trainee t);
+
+        IEnumerator<Trainee> GetAllTrainee();
+        IEnumerator<Tester> GetAllTesters();
+        IEnumerator<Test> GetAllTests();
+    }
+}
