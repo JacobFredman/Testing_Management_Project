a4b198c1d08aebab798d6fc22af4b9b90da31b21
jacob <jacob@app2q.com>
2018-11-21 16:25:17 +0200

    functions in IBL

---
+++ BL/BlImp.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+
+namespace BL
+{
+    public class BlImp : IBL
+    {
+        public void AddTester(Tester newTester)
+        {
+            if (GetAge(newTester.BirthDate) < 40) 
+                return;
+        }
+
+        private static int GetAge(DateTime birthDate)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - birthDate.Year;
+            if (birthDate > today.AddYears(-age)) age--;
+            return age;
+        }
+
+        public void RemoveTester(Tester testerToDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTester(Tester updatedTester)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTest(Test newTest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTest(Test testToDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTest(Test updatedTest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTrainee(Trainee newTrainee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTrainee(Trainee traineeToDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTrainee(Trainee updatedTrainee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Trainee> GetAllTrainee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Tester> GetAllTesters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Test> GetAllTests()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BL/IBL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using DS;
+
+namespace BL
+{
+    internal interface IBL
+    {
+        void AddTester(Tester newTester);
+        void RemoveTester(Tester testerToDelete);
+        void UpdateTester(Tester updatedTester);
+
+        void AddTest(Test newTest);
+        void RemoveTest(Test testToDelete);
+        void UpdateTest(Test updatedTest);
+
+        void AddTrainee(Trainee newTrainee);
+        void RemoveTrainee(Trainee traineeToDelete);
+        void UpdateTrainee(Trainee updatedTrainee);
+
+        IEnumerator<Trainee> GetAllTrainee();
+        IEnumerator<Tester> GetAllTesters();
+        IEnumerator<Test> GetAllTests();
+    }
+}
