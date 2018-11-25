6bf1da52a21899e10f1b8032c3e6210988a19933
jacob <jacob@app2q.com>
2018-11-25 18:07:34 +0200

    no change

--- BL/BlImp.cs
+++ BL/BlImp.cs
@@ -35,9 +35,9 @@ namespace BL
             var traineeExist = AllTrainee.Any(trainee => trainee.ID == newTest.TraineeId);
             var twoTestesTooClose = AllTests.Any(test => (test.TraineeId == newTest.TraineeId) && ((newTest.Date - test.Date).TotalDays < Configuration.MinTimeBetweenTests));
             var lessThenMinLessons = AllTrainee.Any(trainee => (trainee.ID == newTest.TraineeId) && trainee.NumberOfLessons < Configuration.MinLessons);
-            var TraineehasLicense = AllTrainee.Any(trainee =>
+            var traineehasLicense = AllTrainee.Any(trainee =>
                 (trainee.ID == newTest.TesterId) && (trainee.LicenceType.Any(l => l == newTest.LicenceType)));
-            var TesterHasLicecnce = AllTesters.Any(tester => 
+            var testerHasLicecnce = AllTesters.Any(tester => 
                 (tester.ID == newTest.TesterId) && (tester.LicenceType.Any(l => l == newTest.LicenceType)));
 
             var traineeHasTestInSameTime = AllTests.Any(test => (test.TraineeId == newTest.TraineeId) && (newTest.Date == test.Date));
@@ -46,8 +46,8 @@ namespace BL
             if(!traineeExist) throw new Exception("this trainee doesn't exist");
             if (twoTestesTooClose) throw  new Exception("the trainee has a test less then a week ago");
             if(lessThenMinLessons) throw new  Exception("the trainee learned less then " + Configuration.MinLessons + " lessons which is the minimum");
-            if(TraineehasLicense) throw  new Exception("the trainee has already a license with same type");
-            if (!TesterHasLicecnce) throw new Exception("tester is not qualified for this licence type");
+            if(traineehasLicense) throw  new Exception("the trainee has already a license with same type");
+            if (!testerHasLicecnce) throw new Exception("tester is not qualified for this license type");
             if(traineeHasTestInSameTime) throw  new Exception("the trainee has already another test in the same time");
             if(testerHasTestInSameTime) throw  new Exception("the tester has already another test in the same time");
 
