10e202149e4ecd8e953bf8dd83fe6c1efa5db3c0
ElishaMayer <44092325+ElishaMayer@users.noreply.github.com>
2018-11-27 11:05:35 +0200

    factory change name

---
+++ BL/FactoryBL.cs
@@ -0,0 +1,18 @@
+﻿namespace BL
+{
+    public static class FactoryBL
+    {
+        private static BlImp _bl = null;
+
+        public static BlImp GetObject
+        {
+            get
+            {
+                if (_bl == null)
+                    _bl = new BlImp();
+                return _bl;
+            }
+        }
+
+    }
+}
