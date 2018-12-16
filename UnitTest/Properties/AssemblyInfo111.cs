f97767b248b8ed4d0cf08ae4ff76d3a5bd132918
יעקב פרדמן <jacov141@gmail.com>
2018-12-16 02:55:01 +0200

    Unitest project replaced

--- UnitTest/Properties/AssemblyInfo111.cs
+++ UnitTest/Properties/AssemblyInfo111.cs
@@ -1,19 +1,18 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyTitle("UnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyProduct("UnitTest")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("bd338fce-6c80-408d-a4c2-98a1f799dcbc")]
+[assembly: Guid("20301ee2-f001-4d5e-9ed1-586957a40723")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
