622f7f3ca6a5eb233f1d97d37034816286361615
יעקב פרדמן <jacov141@gmail.com>
2018-12-27 00:24:51 +0200

    not for use

--- PLWPF/MainWindow.xaml.cs
+++ PLWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -9,7 +10,6 @@ using BE.Routes;
 using BL;
 using MahApps.Metro.Controls;
 using PLWPF.Admin;
-using PLWPF.TraineeArea;
 
 namespace PLWPF
 {
@@ -25,7 +25,7 @@ namespace PLWPF
         {
             InitializeComponent();
 
-     
+
 
             //Add information to test the program
             AddInfo();
@@ -39,7 +39,7 @@ namespace PLWPF
             Configuration.firtOpenProgram = false;
         }
 
-   
+
 
         /// <summary>
         ///     for debugging only !!!!!!!!!!!!!!!!!!!!!!
@@ -48,46 +48,40 @@ namespace PLWPF
         {
             try
             {
-                var test = new LessonsAndType {License = LicenseType.A, NumberOfLessons = 30, ReadyForTest = true};
+                var test = new LessonsAndType { License = LicenseType.A, NumberOfLessons = 30, ReadyForTest = true };
                 var list = new List<LessonsAndType>();
                 list.Add(test);
                 _blimp.AddTrainee(new Trainee(319185997, Gender.Male, "Elisha", "Mayer")
                 {
-                    BirthDate = new DateTime(1995, 1, 1), LicenseTypeLearning = list,
+                    BirthDate = new DateTime(1995, 1, 1),
+                    LicenseTypeLearning = list,
                     Address = new Address("Jerusalem"),
                     EmailAddress = "elisja.sc@gmail.com"
                 });
-
                 _blimp.AddTrainee(new Trainee(037982519, Gender.Male, "Jacob", "Fredman")
-                {
-                    BirthDate = new DateTime(1985, 12, 29),
-                    LicenseTypeLearning = list,
-                    Address = new Address("Jerusalem"),
-                    EmailAddress = "jacov141@gmail.com"
-                });
-               
+                { BirthDate = new DateTime(1985, 1, 12) });
                 _blimp.AddTrainee(new Trainee(319185971, Gender.Male, "Moshe", "Levi")
-                    {BirthDate = new DateTime(1987, 1, 1)});
+                { BirthDate = new DateTime(1987, 1, 1) });
                 _blimp.AddTrainee(new Trainee(314661133, Gender.Male, "Bob", "Ray")
-                    {BirthDate = new DateTime(1980, 1, 1)});
+                { BirthDate = new DateTime(1980, 1, 1) });
                 _blimp.AddTrainee(new Trainee(324126747, Gender.Male, "Avi", "Alon")
-                    {BirthDate = new DateTime(1970, 1, 1)});
+                { BirthDate = new DateTime(1970, 1, 1) });
                 _blimp.AddTrainee(new Trainee(326591088, Gender.Female, "Avia", "Abu")
-                    {BirthDate = new DateTime(1999, 1, 1)});
+                { BirthDate = new DateTime(1999, 1, 1) });
                 _blimp.AddTrainee(new Trainee(342533643, Gender.Male, "Gil", "Rami")
-                    {BirthDate = new DateTime(2000, 1, 1)});
+                { BirthDate = new DateTime(2000, 1, 1) });
                 _blimp.AddTrainee(new Trainee(339794166, Gender.Male, "David", "Aboulafia")
-                    {BirthDate = new DateTime(2000, 1, 1)});
+                { BirthDate = new DateTime(2000, 1, 1) });
                 _blimp.AddTrainee(new Trainee(336390885, Gender.Male, "Shlomo", "Simchon")
-                    {BirthDate = new DateTime(1991, 1, 1)});
+                { BirthDate = new DateTime(1991, 1, 1) });
                 _blimp.AddTrainee(new Trainee(332484609, Gender.Female, "Gavriela", "Abuxsis")
-                    {BirthDate = new DateTime(1991, 1, 1)});
+                { BirthDate = new DateTime(1991, 1, 1) });
                 _blimp.AddTrainee(new Trainee(332307065, Gender.Female, "Yafa", "Alaluf")
-                    {BirthDate = new DateTime(1999, 1, 1)});
+                { BirthDate = new DateTime(1999, 1, 1) });
                 _blimp.AddTrainee(new Trainee(332270446, Gender.Male, "Dudu", "Tapiro")
-                    {BirthDate = new DateTime(2000, 1, 1)});
+                { BirthDate = new DateTime(2000, 1, 1) });
                 _blimp.AddTrainee(new Trainee(329043459, Gender.Male, "Pinchas", "Moshe")
-                    {BirthDate = new DateTime(1988, 1, 1)});
+                { BirthDate = new DateTime(1988, 1, 1) });
 
                 var sch = new WeekSchedule();
                 sch.AddHoursAllDays(12, 15);
@@ -95,48 +89,41 @@ namespace PLWPF
                 list2.Add(LicenseType.A);
                 _blimp.AddTester(new Tester(328729660, "meir", "")
                 {
-                    BirthDate = new DateTime(1945, 1, 1), Address = new Address("בית שמש"), MaxDistance = 10000000,
-                    Schedule = sch, MaxWeekExams = 10, LicenseTypeTeaching = list2
+                    BirthDate = new DateTime(1945, 1, 1),
+                    Address = new Address("בית שמש"),
+                    MaxDistance = 10000000,
+                    Schedule = sch,
+                    MaxWeekExams = 10,
+                    LicenseTypeTeaching = list2
                 });
                 sch = new WeekSchedule();
                 sch.AddHoursAllDays(0, 23);
                 _blimp.AddTester(new Tester(324040443, "Maoz,", "Shectman")
-                    {BirthDate = new DateTime(1970, 1, 1), Address = new Address("Tel Aviv"), Schedule = sch});
+                { BirthDate = new DateTime(1970, 1, 1), Address = new Address("Tel Aviv"), Schedule = sch });
                 sch = new WeekSchedule();
                 sch.AddHoursAllDays(0, 23);
                 _blimp.AddTester(new Tester(323873182, "Eliran", "Franko")
-                    {BirthDate = new DateTime(1961, 1, 1), Address = new Address("Beit shemesh"), Schedule = sch});
+                { BirthDate = new DateTime(1961, 1, 1), Address = new Address("Beit shemesh"), Schedule = sch });
                 sch = new WeekSchedule();
                 sch.AddHoursAllDays(0, 23);
                 _blimp.AddTester(new Tester(323082321, "David", "Arbiv")
-                    {BirthDate = new DateTime(1950, 1, 1), Address = new Address("Holon"), Schedule = sch});
+                { BirthDate = new DateTime(1950, 1, 1), Address = new Address("Holon"), Schedule = sch });
 
                 var test1 = new Test(328729660, 319185997)
                 {
                     TestTime = new DateTime(2018, 12, 18, 12, 00, 00),
-                   AddressOfBeginningTest = new Address("jerusalem"),
-                    LicenseType = LicenseType.A
-                };
-
-                var test2 = new Test(328729660, 037982519)
-                {
-                    TestTime = new DateTime(2018, 12, 19, 12, 00, 00),
                     AddressOfBeginningTest = new Address("jerusalem"),
                     LicenseType = LicenseType.A
                 };
                 try
                 {
-                  //  test1.SetRouteAndAddressToTest(new Address("jerusalem"));
+                    //  test1.SetRouteAndAddressToTest(new Address("jerusalem"));
                 }
                 catch
                 {
-
                 }
 
-                test2.Passed = true;
-               // _blimp.AddTest(test1);
-                _blimp.AddTest(test2);
-                _blimp.SendEmailToAllTraineeAfterTest();
+                _blimp.AddTest(test1);
             }
             catch (Exception ex)
             {
@@ -243,7 +230,7 @@ namespace PLWPF
                 AdminUsernameTextBox.Text = "";
                 AdminPasswordTextBox.Password = "";
                 Hide();
-                var win =new Administrator();
+                var win = new Administrator();
                 win.ShowDialog();
                 Show();
             }
